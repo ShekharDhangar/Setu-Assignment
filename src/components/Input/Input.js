ab19c030c5a4731a1e5161029b65fbbff2e027f8
Shekhar Dhangar <shekhardhangar@yahoo.com>
2022-07-10 11:23:44 +0530

    refactor: Input Component
    Refactored Input component to use controlled input.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,20 +2,14 @@ import React from 'react';
 import styles from './input.module.css';
 import { validator } from '../../utils/validator';
 
-function Input({ validateMessage = 'Invalid Input', label, onChange, ...props }) {
+function Input({ validateMessage = 'Invalid Input', label, inputUpdate, ...props }) {
   const [error, setError] = React.useState(false);
-
-  const handleChange = (event) => {
-    const { value } = event.target;
-    onChange(value);
-  };
 
   const handleBlur = (value) => !(props.type === 'text') && setError(() => validator[props?.type](value));
   return (
     <div className={styles.inputContainer}>
       {label && <label htmlFor="input">{label}</label>}
-
-      <input className={styles.input} onBlur={(e) => handleBlur(e.target.value)} {...props} onChange={handleChange} />
+      <input className={styles.input} onBlur={(e) => handleBlur(e.target.value)} {...props} onChange={inputUpdate} />
       {error && <span className={styles.errorControl}>{validateMessage}</span>}
     </div>
   );
